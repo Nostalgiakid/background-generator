8529f08ed352e5ad97e8f7c99f5af81fde3a94e4
bennie <>
2023-03-06 21:02:41 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,32 @@
+//create selecror 
+
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient")
+
+
+function setGradient(){
+    body.style.background =
+    " linear-gradient( to right, "
+    + color1.value
+     +"," 
+     + color2.value 
+     + ")";
+
+     css.textContent = body.style.background + ","
+
+}
+
+
+color1.addEventListener("input",setGradient)
+   
+color2.addEventListener("input",setGradient);
+
+function showMessage() {
+    alert("This page is all about changing background");
+  }
+
+
+
+
